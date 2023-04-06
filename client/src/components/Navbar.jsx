0bc3851cffbd8c6006bf27ae5db2c2d8901c3957
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-04-06 10:36:21 -0700

    updated nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function NavigationBar() {
   return (
@@ -10,11 +10,11 @@ function NavigationBar() {
           <Navbar.Brand href="/">Gamer Thoughts</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/postreview">Create Post</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/discussion">Dicussions</Nav.Link>
+            <Nav.Link href="/contactus">Contact Us</Nav.Link>
           </Nav>
         </Container>
-      </Navbar> 
+      </Navbar>
     </>
   );
 }
