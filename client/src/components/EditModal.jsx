117f2c55d29191667f1425499dec7417346a710a
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-04-06 10:36:12 -0700

    removed unused code

--- client/src/components/EditModal.jsx
+++ client/src/components/EditModal.jsx
@@ -1,35 +1,16 @@
-/**
- * 
-  //A function to handle the post request
-  const putReview = (toEditReview) => {
-    return fetch(`http://localhost:8080/reviews/${toEditReview.post_id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(toEditReview),
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        onUpdateReview(data);
-        //this line just for cleaning the form
-        clearForm();
-      });
-  };
- */
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import EditingForm from './EditForm';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import EditingForm from "./EditForm";
 
-function EditModal({gameReview, updateReview}) {
+function EditModal({ gameReview, updateReview }) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
-    <div className='EditForm' >
+    <div className="EditForm">
       <Button bsPrefix="btn" variant="primary" onClick={handleShow}>
         Edit Post
       </Button>
@@ -37,7 +18,13 @@ function EditModal({gameReview, updateReview}) {
         <Modal.Header closeButton>
           <Modal.Title>Edit Game Review</Modal.Title>
         </Modal.Header>
-        <Modal.Body><EditingForm review={gameReview} updateReview={updateReview} handleClose={handleClose}/></Modal.Body>
+        <Modal.Body>
+          <EditingForm
+            review={gameReview}
+            updateReview={updateReview}
+            handleClose={handleClose}
+          />
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
