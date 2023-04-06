9c8778dcc9b8cb0495c5de857248330781413a25
Ruby Celeste Marroquin <89949645+rubycmarroquin@users.noreply.github.com>
2023-04-06 10:36:00 -0700

    added login component to creating a post

--- client/src/pages/ReviewForm.jsx
+++ client/src/pages/ReviewForm.jsx
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
+import Login from "../components/Login";
 import { Button, Form } from "react-bootstrap";
 
-const ReviewForm = ( ) => {
+const ReviewForm = () => {
+  const [showForm, setShowForm] = useState(false);
+  const [user, setUser] = useState("");
+
   // This is the original State with no initial blog content
-  const [gameReview, setGameReview] = useState(
-     {
-      game_name: "",
-      title: "",
-      console: "",
-      rating: "",
-      recommendation: false,
-      username: "",
-      post: "",
-      image_url: "",
-    }
-  );
+  const [gameReview, setGameReview] = useState({
+    game_name: "",
+    title: "",
+    console: "",
+    rating: "",
+    recommendation: false,
+    username: "",
+    post: "",
+    image_url: "",
+  });
 
   //create functions that handle the event of the user typing into the form
   const handleGameName = (event) => {
@@ -69,6 +71,7 @@ const ReviewForm = ( ) => {
 
   //A function to handle the post request
   const postReview = (newReview) => {
+    newReview.username = user;
     return fetch("http://localhost:8080/reviews", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -89,101 +92,108 @@ const ReviewForm = ( ) => {
   };
 
   return (
-    <div className="CreateFormDiv">
-    <h1 className="CreateFormTitle">Create Blog Post</h1>
-    <Form className="form-students" onSubmit={handleSubmit}>
-      <Form.Group>
-        <Form.Label>Game Title: </Form.Label>
-        <input
-          type="text"
-          id="add-game-name"
-          placeholder="Game Title"
-          required
-          value={gameReview.game_name || ""}
-          onChange={handleGameName}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Title: </Form.Label>
-        <input
-          type="text"
-          id="add-tile"
-          placeholder="Post Title"
-          required
-          value={gameReview.title || ""}
-          onChange={handleTitle}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Console</Form.Label>
-        <input
-          type="text"
-          id="add-console"
-          placeholder="Console / Device Played"
-          required
-          value={gameReview.console || ""}
-          onChange={handleConsole}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Rating: </Form.Label>
-        <input
-          type="text"
-          id="add-rating"
-          placeholder="Rating (out of 5)"
-          required
-          value={gameReview.rating || ""}
-          onChange={handleRating}
-        />
-      </Form.Group>
-      <Form.Check
-        type={"checkbox"}
-        id={`isRecommended`}
-        checked={gameReview.recommendation || false}
-        onChange={handleRecommendation}
-        label={`Do you recommend this game?`}
-      />
-      <Form.Group>
-        <Form.Label>Username: </Form.Label>
-        <input
-          type="text"
-          id="add-username"
-          placeholder="Username"
-          required
-          value={gameReview.username || ""}
-          onChange={handleUsername}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Blog Post: </Form.Label>
-        <input
-          type="text"
-          id="add-post"
-          placeholder="What are your thoughts?"
-          required
-          value={gameReview.post || ""}
-          onChange={handlePostText}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Image URL:</Form.Label>
-        <input
-          type="text"
-          id="add-image"
-          placeholder="Enter Image URL"
-          required
-          value={gameReview.image_url || ""}
-          onChange={handleImageURL}
-        />
-      </Form.Group>
-      <br/>
-      <Form.Group>
-        <Button type="submit" variant="outline-success">
-          Create Post
-        </Button>
-      </Form.Group>
-    </Form>
-    </div>
+    <>
+      {showForm ? (
+        <div className="CreateFormDiv">
+          <h1 className="CreateFormTitle">Create Blog Post</h1>
+          <Form className="form-students" onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>Game Title: </Form.Label>
+              <input
+                type="text"
+                id="add-game-name"
+                placeholder="Game Title"
+                required
+                value={gameReview.game_name || ""}
+                onChange={handleGameName}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Title: </Form.Label>
+              <input
+                type="text"
+                id="add-tile"
+                placeholder="Post Title"
+                required
+                value={gameReview.title || ""}
+                onChange={handleTitle}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Console</Form.Label>
+              <input
+                type="text"
+                id="add-console"
+                placeholder="Console / Device Played"
+                required
+                value={gameReview.console || ""}
+                onChange={handleConsole}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Rating: </Form.Label>
+              <input
+                type="text"
+                id="add-rating"
+                placeholder="Rating (out of 5)"
+                required
+                value={gameReview.rating || ""}
+                onChange={handleRating}
+              />
+            </Form.Group>
+            <Form.Check
+              type={"checkbox"}
+              id={`isRecommended`}
+              checked={gameReview.recommendation || false}
+              onChange={handleRecommendation}
+              label={`Do you recommend this game?`}
+            />
+            <Form.Group>
+              <Form.Label>Username: </Form.Label>
+              <input
+                type="text"
+                id="add-username"
+                placeholder="Username"
+                disabled
+                required
+                value={user || ""}
+                onChange={handleUsername}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Blog Post: </Form.Label>
+              <input
+                type="text"
+                id="add-post"
+                placeholder="What are your thoughts?"
+                required
+                value={gameReview.post || ""}
+                onChange={handlePostText}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Image URL:</Form.Label>
+              <input
+                type="text"
+                id="add-image"
+                placeholder="Enter Image URL"
+                required
+                value={gameReview.image_url || ""}
+                onChange={handleImageURL}
+              />
+            </Form.Group>
+            <br />
+            <Form.Group>
+              <Button type="submit" variant="outline-success">
+                Create Post
+              </Button>
+            </Form.Group>
+          </Form>
+        </div>
+      ) : (
+        <Login setShowForm={setShowForm} setUser={setUser} />
+      )}{" "}
+    </>
   );
 };
 
